d376f1afb1de109a607f09074364ecc472ffec04
Carrascoj1998 <carrascoj1998@gmail.com>
2023-07-11 18:25:26 -0500

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import {Routes, Route} from "react-router-dom";
-import Layout from "./components/layout/Layout";
-import MainPage from "./pages/MainPage";
-import ContactResume from "./pages/Contact&Resume";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import MainPage from './pages/MainPage';
+import ContactResume from './pages/Contact&Resume';
 import { Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <Container>
-        <Layout>
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/contact" element={<ContactResume />} />
-            </Routes>
-        </Layout>
-    </Container>
-  );
+    const navigate = useNavigate();
+
+
+    const redirectToHome = () => {
+        navigate('/');
+    };
+
+    return (
+        <Container>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/contact" element={<ContactResume />} />
+                    <Route path="*" element={redirectToHome} />
+                </Routes>
+            </Layout>
+        </Container>
+    );
 }
 
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <BrowserRouter>
+ReactDOM.render(
+    <Router>
         <App />
-    </BrowserRouter>
-
+    </Router>,
+    document.getElementById('root')
 );
-
 
