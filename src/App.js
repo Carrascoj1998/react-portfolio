c39ef0d03b4a4920c75f637bfd08cb73196e6f41
Carrascoj1998 <carrascoj1998@gmail.com>
2023-07-11 14:00:32 -0500

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import MainPage from './pages/MainPage';
-import ContactResume from './pages/Contact&Resume';
+import {Routes, Route} from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import MainPage from "./pages/MainPage";
+import ContactResume from "./pages/Contact&Resume";
 import { Container } from 'react-bootstrap';
 
 function App() {
-    return (
-        <Container>
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/contact" element={<ContactResume />} />
-                </Routes>
-            </Layout>
-        </Container>
-    );
+  return (
+    <Container>
+        <Layout>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/contact" element={<ContactResume />} />
+            </Routes>
+        </Layout>
+    </Container>
+  );
 }
-
-ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
-);
 
 export default App;
